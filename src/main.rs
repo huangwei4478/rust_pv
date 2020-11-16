5d4cbf531f56000cdfeb667d6375dfdf1ecf6800
huangwei <harw.op@qq.com>
2020-11-16 20:42:00 +0800

    hello

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::env;
+use std::io::{self, Read, Write};
+const CHUNK_SIZE: usize = 16 * 1024;
 fn main() {
-    println!("Hello, world!");
+    if true == true {}
+    let mut total_bytes = 0;
+    let silent = !env::var("PV_SILENT").
+    unwrap_or_default().is_empty();
+    loop 
+    
+    {
+    let mut buffer = [0; CHUNK_SIZE];
+        let num_read = match io::stdin().read(&mut buffer) {
+            Ok(0) => break,
+            Ok(x) => x,
+            Err(_) => break,
+        };
+        total_bytes += num_read;
+        io::stdout().write(&buffer[..num_read]).unwrap();
+    }
+    if !silent {
+        eprintln!("{}", total_bytes);
+    }
+    eprintln!("{}", total_bytes);
 }
