bbb3e1adf28f3da700775c9b024e6d514000d669
huangwei <harw.op@qq.com>
2020-11-22 11:47:08 +0800

    use crossbeam for higher efficiency

--- src/read.rs
+++ src/read.rs
@@ -1,9 +1,9 @@
 const CHUNK_SIZE: usize = 16 * 1024;
+use crossbeam::channel::Sender;
 use std::fs::File;
 use std::io::{self, BufReader, Read, Result};
-use std::sync::mpsc::Sender;
 
-pub fn read_loop(infile: &str, stats_tx: Sender<Vec<u8>>) -> Result<()> {
+pub fn read_loop(infile: &str, stats_tx: Sender<usize>, write_tx: Sender<Vec<u8>>) -> Result<()> {
     let mut reader: Box<dyn Read> = if infile.is_empty() {
         Box::new(BufReader::new(io::stdin()))
     } else {
@@ -18,11 +18,14 @@ pub fn read_loop(infile: &str, stats_tx: Sender<Vec<u8>>) -> Result<()> {
             Ok(x) => x,
             Err(_) => break,
         };
-        if stats_tx.send(Vec::from(&buffer[..num_read])).is_err() {
+
+        let _ = stats_tx.send(num_read);
+
+        if write_tx.send(Vec::from(&buffer[..num_read])).is_err() {
             break;
         }
     }
-
-    let _ = stats_tx.send(Vec::new());
+    let _ = stats_tx.send(0);
+    let _ = write_tx.send(Vec::new());
     Ok(())
 }
